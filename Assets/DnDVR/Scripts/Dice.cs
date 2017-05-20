bdc52cb677739096debbfe3fe4d8b07b1310dde5
Kirin Patel <kirinpatel@gmail.com>
2017-05-20 14:39:47 -0400

    Added Dice class and testing scene

---
+++ Assets/DnDVR/Scripts/Dice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+    void Update()
+    {
+        // Debug.Log(rollDice());
+    }
+
+    static int rollDice()
+    {
+        return rollDice(20);
+    }
+
+    static int rollDice(int sides)
+    {
+        return Random.Range(0, sides);
+    }
+}
